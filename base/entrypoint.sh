b1c1ed2360987d862dceb90e20c354072e19bde0
Winston <bjdhjj@gmail.com>
2019-07-09 20:17:24 +0800

    use path for ss

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -4,16 +4,17 @@
 sudo ssh-keygen -A
 # prepare xauth
 sudo touch /root/.Xauthority
+sudo mkdir -p /run/nginx
 sudo nginx
 # mark date
-wget https://raw.githubusercontent.com/WinstonH/v2ray-heroku/master/index.html -O /var/lib/nginx/html/index.html
+sudo wget https://raw.githubusercontent.com/WinstonH/v2ray-heroku/master/index.html -O /var/lib/nginx/html/index.html
 VERSION=$(v2ray --version |grep V |awk '{print $2}')
 BUILDDATE=$(v2ray --version |grep V |awk '{print $4}')
 REBOOTDATE=$(date)
 
-sed -i "s/VERSION/$VERSION/g" /var/lib/nginx/html/index.html
-sed -i "s/BUILDDATE/$BUILDDATE/g" /var/lib/nginx/html/index.html
-sed -i "s/REBOOTDATE/$REBOOTDATE/g" /var/lib/nginx/html/index.html
+sudo sed -i "s/VERSION/$VERSION/g" /var/lib/nginx/html/index.html
+sudo sed -i "s/BUILDDATE/$BUILDDATE/g" /var/lib/nginx/html/index.html
+sudo sed -i "s/REBOOTDATE/$REBOOTDATE/g" /var/lib/nginx/html/index.html
 
 sudo /usr/bin/supervisord -c /etc/supervisord.conf
 exec "$@"
