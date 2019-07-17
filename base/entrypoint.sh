bd8021d61350c2064159875cf7024efb88d12a04
Winston <bjdhjj@gmail.com>
2019-07-17 14:21:46 +0800

    use php

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -6,15 +6,15 @@ sudo ssh-keygen -A
 sudo touch /root/.Xauthority
 sudo mkdir -p /run/nginx
 sudo nginx
+sudo php-fpm7
 # mark date
-sudo wget https://raw.githubusercontent.com/WinstonH/v2ray-heroku/master/index.html -O /var/lib/nginx/html/index.html
 VERSION=$(sudo /usr/bin/v2ray/v2ray --version |grep V |awk '{print $2}')
 BUILDDATE=$(sudo /usr/bin/v2ray/v2ray --version |grep V |awk '{print $4}')
 REBOOTDATE=$(date)
 
-sudo sed -i "s/VERSION/$VERSION/g" /var/lib/nginx/html/index.html
-sudo sed -i "s/BUILDDATE/$BUILDDATE/g" /var/lib/nginx/html/index.html
-sudo sed -i "s/REBOOTDATE/$REBOOTDATE/g" /var/lib/nginx/html/index.html
+sudo sed -i "s/VERSION/$VERSION/g" /home/alpine/www/index.html
+sudo sed -i "s/BUILDDATE/$BUILDDATE/g" /home/alpine/www/index.html
+sudo sed -i "s/REBOOTDATE/$REBOOTDATE/g" /home/alpine/www/index.html
 
 sudo /usr/bin/supervisord -c /etc/supervisord.conf
 exec "$@"
